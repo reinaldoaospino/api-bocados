f8ac24c2624dc8cbf7b4c775e08dd36a89b2a54c
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-10-17 20:15:53 -0300

    primer approach api

---
+++ api-bocados/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_bocados.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthController
+    {
+
+        //[AllowAnonymous]
+        //[HttpPost]
+        //[Route("token")]
+        //public async Task<IActionResult> GetToken(TokenRequestModel request)
+        //{
+        //    var tokenDto = _mapper.Map<TokenRequest>(request);
+        //    var token = await _tokenManager.GetToken(tokenDto);
+        //    var tokenReponse = _mapper.Map<TokenResponseModel>(token);
+
+        //    return Ok(tokenReponse);
+        //}
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> CreateAuth(AuthUserModel authUserModel)
+        //{
+        //    var authUserDto = _mapper.Map<AuthUser>(authUserModel);
+        //    await _tokenManager.Create(authUserDto);
+        //    return Ok();
+        //}
+    }
+}
