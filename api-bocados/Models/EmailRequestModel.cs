322ec41308596cbc86bc68ec788322856e19fefd
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-05 14:31:33 -0300

    Create EmailRequestModel.cs

---
+++ api-bocados/Models/EmailRequestModel.cs
@@ -0,0 +1,15 @@
+namespace api_bocados.Models
+{
+    public class EmailRequestModel
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddres { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
