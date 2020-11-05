0242ab8d8c457a5f6ce1cccbb719a36752a384b1
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-05 14:33:15 -0300

    Create Email.cs

---
+++ Domain/Entities/Email.cs
@@ -0,0 +1,15 @@
+namespace Domain.Entities
+{
+    public class Email
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddres { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
